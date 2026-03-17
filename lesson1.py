7147cdab6538dd675add53de36df9b99b9c1f781
fronexxx <igorbotsii42@gmail.com>
2026-03-17 22:30:23 +0200

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+# print("Hello from Python")
+#
+# print(1, 2, 'HELLO', sep='-', end='')
+
+i = 1
+f = 1.3
+b = True
+s = 'text'
+n = None
+
+print(type(i))
+print(type(f))
+print(type(b))
+print(type(s))
+print(type(n))
