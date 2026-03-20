41f5dd2dfbb6c88040a3cfa793bde3e0ec383337
fronexxx <igorbotsii42@gmail.com>
2026-03-20 12:55:56 +0200

    homework1.py

---
+++ homework1.py
@@ -0,0 +1,64 @@
+# Task-1
+# st = 'as 23fdfdg544'
+#
+# print(', '.join((num for num in st if num.isdigit())))
+
+# Task-2
+# st = 'as 23fdfdg544'
+#
+# print(', '.join(''.join(ch if ch.isdigit() else ' ' for ch in st).split()))
+
+# Task-3
+# greeting = 'Hello, world'
+#
+# print(str([g for g in greeting]).upper())
+
+# Task-4
+
+# print([num**2 for num in  range(50) if num%2])
+
+
+# Task-5
+# def printer (list_):
+#     for i in list_:
+#         print(i)
+#
+# printer([1, 2, 4, 5, 6, 7, 8, 9, 9])
+
+# Task-6
+# def max_number(a, b, c):
+#     print(max(a, b, c))
+#     return max(a, b, c)
+#
+# max_number(33, 44, 6)
+
+# Task-7
+# def numbers(*args):
+#     print(max(args))
+#     return min(args)
+#
+# s = numbers(44, 3, 6, 22)
+# print(s)
+
+# Task-8
+# def max_from_list(list_):
+#     return max(list_)
+#
+# def min_from_list(list_):
+#     return min(list_)
+
+# Task-9
+# def sum_list(list_):
+#     return sum(list_)
+#
+#
+# i = sum_list([2, 3, 5, 2, 5])
+# print(i)
+
+# Task-10
+# def avg (list_):
+#     return sum(list_)/len(list_)
+#
+#
+# res = avg([2, 3, 2, 1, 5, 7, 1])
+# print(int(res))
