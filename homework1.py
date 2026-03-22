897f59b9f4e53f480cac03a5b72c0165742d4ee0
fronexxx <igorbotsii42@gmail.com>
2026-03-22 12:59:15 +0200

    homework1.py

--- homework1.py
+++ homework1.py
@@ -62,3 +62,83 @@
 #
 # res = avg([2, 3, 2, 1, 5, 7, 1])
 # print(int(res))
+
+# list = [22, 3,5,2,8,2,-23, 8,23,5]
+#
+#   – знайти  мін. число
+#
+#   – видалити усі дублікати
+#
+#   – замінити кожне 4-те значення на ‘X’
+#
+# 2) вивести на екран пустий квадрат з “*”, сторона якого вказана як аргумент функції
+#
+# 3) вивести табличку множення за допомогою циклу while
+#
+# 4) переробити це завдання під меню
+#
+
+
+l1 = [22, 3,5,2,8,2,-23, 8,23,5]
+# copy = l1.copy()
+# print(min(copy))
+def min_from_list(list_):
+    print(min(list_))
+
+def duplicate ():
+    copy = l1.copy()
+    print(list(set(copy)))
+# duplicate()
+
+def to_x():
+    copy = l1.copy()
+    print(['X' if not (i + 1)%4 else num for i, num in enumerate(copy)])
+# to_x()
+
+def square(n):
+    for i in range(n):
+        if i == 0 or i == n - 1:
+            print('*'*n)
+        else: print('*' + ' '* (n - 2) + '*')
+# square(5)
+
+def multi_table():
+    size = 9
+    i = 1
+    while i <= size:
+        j = 1
+        while j <= size:
+            res = i * j
+            # print(' ' if res//10 else '  ', end='')
+            # print(res, end='')
+            print(f'{res: 3}', end='')
+            j += 1
+        print()
+        i+=1
+#
+# multi_table()
+
+
+while True:
+    print('1) знайти  мін. число')
+    print('2) видалити усі дублікати')
+    print('3) замінити кожне 4-те значення на X')
+    print('4) вивести на екран пустий квадрат з “*”, сторона якого вказана як аргумент функції')
+    print('5) вивести табличку множення за допомогою циклу while')
+    print('9) Вихід')
+
+    choice = input('Зробіть свій вибір: ')
+
+    if choice == '1':
+        min_from_list(l1)
+    elif choice == '2':
+        duplicate()
+    elif choice == '3':
+        to_x()
+    elif choice == '4':
+        square(7)
+    elif choice == '5':
+       multi_table()
+    elif choice == '9':
+        break
+
